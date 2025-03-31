5d66e79c716c34f6d08105d89ad84af85460a161
hija-happy <hijahappy2004@gmail.com>
2025-03-31 14:27:28 +0530

    added cards

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import './Home.css'
 import Navbar from '../../components/Navbar'    
+import HeroPic from '../../assets/images/movie_banner3.jpg'
+import HeroTitle from '../../assets/images/movie_title.png'
+import HeroNSeries from '../../assets/images/n-series.png';
 const Home = () => {
   return (
-    <div className=''>
+    <div className='home'>
         <Navbar/>
+        <div className="hero">
+          <img src={HeroPic} alt="" className='hero-pic'/>
+          <div className="hero-description">
+            <img src={HeroNSeries} alt="" className='hero-n-series' />
+            <img src={HeroTitle}alt="" className='hero-title'/>
+            <p className='movie-info'>
+            Mutants fight for survival in a world that fears them. Led by Professor X, the X-Men battle powerful enemies like Magneto to protect both humans and mutants.
+            </p>
+             <div className="hero-buttons">
+              <button className='play'> &#9658; Play </button>
+              <button className='my-list'> + My List</button>
+             </div>
+          </div>
+        </div>
     </div>
   )
 }
