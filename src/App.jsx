063356b0e0d261f4827da99a4492c89e0a3d263c
hija-happy <hijahappy2004@gmail.com>
2025-04-05 20:48:16 +0530

    Added Landing Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Landing from './pages/Landing-Page/Landing';
 import Home from './pages/Home/Home'
 import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login/Login';
@@ -8,7 +9,8 @@ const App = () => {
   return (
     <div>
    <Routes>
-  <Route path="/" element={<Login />} />          
+    <Route path='/' element={<Landing />} />
+  <Route path="/login" element={<Login />} />          
   <Route path="/home" element={<Home />} />       
   <Route path="/player/:id" element={<Player />} />
 </Routes>
